921e17017f7ef50853f8d5d20348e9036598bee3
Makoto Hiramatsu <s1311536@u.tsukuba.ac.jp>
2015-02-19 17:02:58 +0900

    update 0004 but I don't get it

--- Volume0/0004.rb
+++ Volume0/0004.rb
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+# ガウス・ジョルダンの消去法を使った実装も後でする
 
-a, b, c, d, e, f = gets.chomp.split(" ").map{|item| item.to_f}
+def solve(a, b, c, d, e, f)
+  x = (e*c-b*f)/(a*e-b*d)
+  y = (a*f-c*d)/(a*e-b*d)
+  return x, y
+end
 
-x = (c-f) / (a-d)
-y = (c-a)/b * x
-
-puts x
-puts y
+while line = gets
+  a, b, c, d, e, f = line.chomp.split(" ").map{|item| item.to_f}
+  result = solve(a, b, c, d, e, f)
+  printf("%.3f %.3f", result[0]+0, result[1]+0)
+end
