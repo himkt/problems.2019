d5c8f29c05339b24c1d5050b74b5db64a772ffc9
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2015-04-08 14:04:20 +0900

    add 1-1

---
+++ ari/1/1.cpp
@@ -0,0 +1,31 @@
+# include <cstdio>
+
+const int MAX_N = 50;
+
+int main(){
+  int n, m, k[MAX_N];
+
+  scanf("%d %d", &n, &m);
+  for(int i=0; i<n; i++){
+    scanf("%d", &k[i]);
+  }
+
+  bool f = false;
+
+  for(int a=0; a<n; a++){
+    for(int b=0; b<n; b++){
+      for(int c=0; c<n; c++){
+        for(int d=0; d<n; d++){
+          if(k[a]+k[b]+k[c]+k[d]==m){
+            f = true;
+          }
+        }
+      }
+    }
+  }
+  if (f){
+    printf("Yes\n");
+  }else{
+    printf("No\n");
+  }
+}
