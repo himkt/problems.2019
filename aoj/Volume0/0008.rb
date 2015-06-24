19236e40f46a571e989a0c1a73db8283b95ab0c6
himkt <himkt@klis.tsukuba.ac.jp>
2015-06-24 16:41:06 +0900

    create: 0009(aoj)

--- aoj/Volume0/0008.rb
+++ aoj/Volume0/0008.rb
@@ -2,28 +2,19 @@
 
 
 while n = gets
-
   n = n.chomp.to_i
   ans = 0
 
   i = 0
-  while i<=9
-    j = 0
-    while j<=9
-      k = 0
-      while k<=9
-        l = 0
-        while l<=9
+  for i in (0..9)
+    for j in (0..9)
+      for k in (0..9)
+        for l in (0..9)
           ans+=1 if i+j+k+l == n
-          l+=1
         end
-        k+=1
       end
-      j+=1
     end
-    i+=1
   end
 
   puts ans
-
 end
--- aoj/Volume0/0009.rb
+++ aoj/Volume0/0009.rb
@@ -1,12 +1,4 @@
 require 'prime'
-
 while line = gets
-  n = line.chomp.to_i
-  ans = 0
-  i = 1
-  while i <= n
-    ans += 1 if Prime.prime?(i)
-    i += 1
-  end
-  puts ans
+  puts Prime.each(line.to_i).count
 end
