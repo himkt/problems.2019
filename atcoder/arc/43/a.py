77b5cb4f53ccc5a6de47c41b94ad06a2d5da366b
himkt <himkt@klis.tsukuba.ac.jp>
2015-08-16 07:52:38 +0900

    add: arc43

---
+++ atcoder/arc/43/a.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @author = himkt
+# @create = 2015/08/15
+
+def normalize_score(P,Q):
+    return 0
+
+N,A,B = map(lambda item: int(item), raw_input().split())
+S = []
+
+for i in xrange(N):
+    S.append(int(raw_input()))
+
