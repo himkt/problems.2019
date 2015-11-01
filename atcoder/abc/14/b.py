55dcbf11da3c26624dea76c921a4d730955bbb27
himkt <himkt@klis.tsukuba.ac.jp>
2015-11-01 10:56:34 +0900

    solve abc/14/b

--- atcoder/abc/14/b.py
+++ atcoder/abc/14/b.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-n, X = list(map(int, input().split()))
+n, X = map(int, input().split())
 A = list(map(int, input().split()))
 ans = 0
 
-for i, j in zip(A, format(X, "b")):
-    if (j == "1"):
-        try:
-            ans += i
-        except:
-            break
+for i in range(10000000):
+    shift = X >> i
+    if shift == 0:
+        break
+    if shift % 2 == 1:
+        ans += A[i]
+
 
 print(ans)
