9caa87a8427d65bf202abfa101c932497e90a133
himkt <himkt@klis.tsukuba.ac.jp>
2015-11-13 17:50:54 +0900

    TLE #4

--- atcoder/abc/13/c.py
+++ atcoder/abc/13/c.py
@@ -2,12 +2,20 @@
 
 N, H = list(map(int, input().split()))
 A, B, C, D, E = list(map(int, input().split()))
-cost = 0
 
-for x in range(N):
-    for y in range(N):
-        if H + B*x + D*y - (N-x-y)*E > 0:
-            if x + y == N:
-                print(A * x, C * y)
+cost = 2147483647
+
+for i in range(0, N+1):
+    for j in range(0, N+1):
+        if H + B*i + D*j - (N-i-j)*E > 0:# and N-i-j >= 0:
+
+            # print("H: %s, B: %s, D: %s" % (H, B, D))
+            # print("i: %s, j: %s, k: %s" % (i, j, N-i-j))
+            # print(H + B*i + D*j + (N-i-j)*E)
+            # print("%s-%s-%s" % (i, j, (N-i-j)))
+            # print("cost: %s" % (A*i + C*j))
+
+            if cost > A*i + C*j:
+                cost = A*i + C*j
 
 print(cost)
