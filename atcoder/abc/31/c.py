d25da8e287bed3da407528cf97e02f6f4452d717
himkt <himkt@klis.tsukuba.ac.jp>
2015-11-23 06:46:09 +0900

    refactoring

--- atcoder/abc/31/c.py
+++ atcoder/abc/31/c.py
@@ -9,17 +9,13 @@ for i in range(N):
     for j in range(N):
         if i == j:
             continue
-        if i > j:
-            subA = A[j:i+1]
-        else:
-            subA = A[i:j+1]
 
-        # aoki select left one
-        score.append((sum(subA[1::2]), -j, sum(subA[::2])))
-
+        ii, jj = min(i, j), max(i, j)+1
+        subA = A[ii:jj]
+        # for regulation that aoki select left one
+        score.append((sum(subA[1::2]), -jj, sum(subA[::2])))
     # maximize aoki score
     _, _, ans_i = max(score)
-
     # update answer
     ans = max(ans, ans_i)
 
