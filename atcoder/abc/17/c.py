d30054449cb06715ec3db9c85a760b4b0803650e
himkt <himkt@klis.tsukuba.ac.jp>
2015-11-28 05:34:26 +0900

    solve abc/c/17

--- atcoder/abc/17/c.py
+++ atcoder/abc/17/c.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 N, M = map(int, input().split())
-l, r, s = list(), list(), list()
+im = [0 for _ in range(M+1)]
+
+ans = 0
 
 for _ in range(N):
     elems = list(map(int, input().split()))
-    l.append(elems[0])
-    r.append(elems[1])
-    s.append(elems[2])
+    im[elems[0]-1] += elems[2]
+    im[elems[1]] -= elems[2]
+    ans += elems[2]
 
-print(l, r, s)
+
+for i in range(len(im)):
+    if i+1 < len(im):
+        im[i+1] += im[i]
+
+print(ans - min(im[:-1]))
