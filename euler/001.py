d731ae7ad0697d48501764276f359448481b1efe
himkt <himkt@klis.tsukuba.ac.jp>
2016-02-20 15:29:13 +0900

    tweak

--- euler/001.py
+++ euler/001.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 
 def solve(n):
+    #  It is very useful to use list comprehension
     return sum(i for i in range(n) if i % 3 == 0 or i % 5 == 0)
 
 if __name__ == '__main__':
