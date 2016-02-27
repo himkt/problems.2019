ba09bda4b1bf96005d1b3a56bfb422936e0ddd17
himkt <himkt@klis.tsukuba.ac.jp>
2016-02-27 18:14:18 +0900

    [Euler] Solve 009

---
+++ euler/009.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from functools import reduce
+
+
+def get_pythagorean_triplet(n):
+    return list(filter(lambda x: x[0] < x[1] < x[2] and x[0]**2 + x[1]**2 ==  x[2]**2, [(i, j, 1000-i-j) for i in range(1000) for j in range(1000 - i)]))[0]
+
+
+def solve(n):
+    return reduce(lambda x, y: x * y, get_pythagorean_triplet(n))
+
+if __name__ == '__main__':
+    print(solve(1000))
