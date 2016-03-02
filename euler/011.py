270591037db4554681a9911c678ca98373c6630e
himkt <himkt@klis.tsukuba.ac.jp>
2016-03-02 20:29:17 +0900

    [Euler] Solve 011

--- euler/011.py
+++ euler/011.py
@@ -2,7 +2,18 @@
 
 
 def solve(matrix):
-    print(matrix)
+    ans = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if len(matrix)-1 >= i+3:
+                ans = max(ans, matrix[i][j] * matrix[i+1][j] * matrix[i+2][j] * matrix[i+3][j])
+            if len(matrix[0])-1 >= j+3:
+                ans = max(ans, matrix[i][j] * matrix[i][j+1] * matrix[i][j+2] * matrix[i][j+3])
+            if len(matrix)-1 >= i+3 and len(matrix[0])-1 >= j+3:
+                ans = max(ans, matrix[i][j] * matrix[i+1][j+1] * matrix[i+2][j+2] * matrix[i+3][j+3])
+            if len(matrix)-1 >= i+3 and j-3 >= 0:
+                ans = max(ans, matrix[i][j] * matrix[i+1][j-1] * matrix[i+2][j-2] * matrix[i+3][j-3])
+    return ans
 
 
 if __name__ == '__main__':
@@ -27,6 +38,6 @@ if __name__ == '__main__':
 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'''.split("\n")
+    
     matrix = list(map(lambda row: list(map(int, row.split())), input_string))
-
-    print(matrix)
+    print(solve(matrix))
