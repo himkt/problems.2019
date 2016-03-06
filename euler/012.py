970b57917d7e1619b16eae67d54d1fecf51729ba
himkt <himkt@klis.tsukuba.ac.jp>
2016-03-07 00:06:56 +0900

    [euler] Solve 012 (but have not been checked that is correct)

--- euler/012.py
+++ euler/012.py
@@ -1,41 +1,29 @@
 # -*- coding: utf-8 -*-
 
 
-class TriangleNumber:
+# get_factors :: int -> [int]
+def get_factors(num):
+    factors = list()
 
-    def __init__(self):
-        self.number = 1
-        self.iter_num = 1
+    for i in range(1, num // 2 + 2):
+        if num % i == 0:
+            factors.append(i)
 
-    def next(self):
-        self.iter_num += 1
-        self.number += self.iter_num
-
-    def get_divisors(self):
-        n = self.number
-        divisors = list()
-        for i in range(1, n // 2 + 1):
-            if n % i == 0:
-                divisors.append(i)
-
-        divisors.append(n)
-        return divisors
+    return factors + [num]
 
 
-def solve(n):
-    triangle = TriangleNumber()
-    for _ in range(10000):
-        triangle.next()
-    for _ in range(n):
-        triangle.next()
-        print(triangle.number)
-        print(triangle.get_divisors())
-    # for _ in range(n):
-    #     print(triangle.number)
-    #     triangle.next()
-    #     print(len(triangle.get_divisors()))
-    #     if len(triangle.get_divisors()) >= 500:
-    #         print(triangle.number)
+# triangler_num :: int -> int
+def triangler_num(x):
+    return (x * (x+1)) // 2
+
+
+# solve :: None -> int
+def solve():
+    for x in range(1, 10000000000000000):
+        num = triangler_num(x)
+        if len(get_factors(num)) > 500:
+            return num
+
 
 if __name__ == '__main__':
-    print(solve(100000))
+    print(solve())
