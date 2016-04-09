bb21e9b691d52ef24ab9a23d78db1335018b1fc4
himkt <himkt@klis.tsukuba.ac.jp>
2016-04-09 21:49:26 +0900

    [Typical_DP] Solve a

--- atcoder/typical_dp/a.py
+++ atcoder/typical_dp/a.py
@@ -8,10 +8,11 @@ max_score = sum(P)
 dp = [False] * (max_score + 1)
 dp[0] = True
 
-
 for i in range(N):
     p = P.pop()
     for j in reversed(range(max_score)):
+        if j + p > max_score:
+            continue
         if dp[j]:
             dp[j + p] = True
 
