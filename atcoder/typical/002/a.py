2657f4dc414f2447a006b2f65909c4ac0c4d1ae3
himkt <himkt@klis.tsukuba.ac.jp>
2016-04-20 10:11:34 +0900

    [Typical] Create 002/a #13

---
+++ atcoder/typical/002/a.py
@@ -0,0 +1,10 @@
+R, C = map(int, input().split())
+sy, sx = map(int, input().split())
+gy, gx = map(int, input().split())
+
+c = list()
+
+for _ in range(R):
+    c.append(list(input()))
+
+print(c)
