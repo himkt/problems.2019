6aa41da759e29a9fce01d5d00d0b35c124c9e621
himkt <himkt@klis.tsukuba.ac.jp>
2016-04-20 10:08:48 +0900

    [ARC] Create 042/a #12

--- atcoder/arc/042/a.py
+++ atcoder/arc/042/a.py
@@ -1,12 +1,12 @@
 N, M = map(int, input().split())
-a = list(int(input()) for _ in range(N))
+a = sorted(list(int(input()) for _ in range(N)), reverse=1)
 
-done = list()
+prv = 0
 
 for a_i in reversed(a):
-    if a_i not in done:
+    if prv != a_i:
         print(a_i)
-        done.append(a_i)
+        prv = a_i
 
 for b_i in sorted(set(map(lambda x: x+1, range(N))) - set(a)):
     print(b_i)
