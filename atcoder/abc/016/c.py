2849b25eb5b26a0e522ec9d7c7aa4f315de0e179
himkt <himkt@klis.tsukuba.ac.jp>
2016-04-20 10:09:46 +0900

    [ABC] Create 016/c #6

--- atcoder/abc/016/c.py
+++ atcoder/abc/016/c.py
@@ -1,10 +1,37 @@
 # -*- coding: utf-8 -*-
 
 N, M = map(int, input().split())
-adjacent = [[0 for i in range(N)] for i in range(N)]
 
-for i in range(M):
-    f, t = map(int, input().split())
-    adjacent[f-1][t-1] = 1
+G = list([0 for _ in range(N)] for _ in range(N))
+A = list([0 for _ in range(N)] for _ in range(N))
 
-print(adjacent)
+
+def search(arr, res=[]):
+    if 1 in arr:
+        index = arr.index(1)
+        arr[index] = 0
+        res.append(index)
+        return search(arr, res)
+    return res
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    G[i-1][j-1] = 1
+    G[j-1][i-1] = 1
+
+
+print(G)
+
+for i in range(N):
+    ans = 0
+    for j in range(i):
+        if i == j:
+            continue
+
+        indices_i = search(G[i], [])# | s.t. G[i] = 1
+        indices_j = search(G[j], [])# | s.t. G[j] = 1
+
+        A[i][j] += len(indices_i and indices_j)
+
+
+print(A)
