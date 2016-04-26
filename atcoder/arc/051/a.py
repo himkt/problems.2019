4e45d4e746fbc50d17839dfe63cd1914bf97c9e2
himkt <himkt@klis.tsukuba.ac.jp>
2016-04-26 19:30:09 +0900

    [ARC] Solve 051/a

--- atcoder/arc/051/a.py
+++ atcoder/arc/051/a.py
@@ -2,16 +2,19 @@
 x1, y1, r = map(int, input().split())
 x2, y2, x3, y3 = map(int, input().split())
 
-flag = False
+ans1 = True
+ans2 = False
+
+if x2 <= x1 - r and x1 + r <= x3 and y2 <= y1 - r and y1 + r <= y3:
+    ans1 = False
+
 
 for x in [x2, x3]:
     for y in [y2, y3]:
-        if pow(x1 - x, 2) + pow(y1 - y, 2) <= pow(r, 2):
-            flag = True
+        if pow(x1 - x, 2) + pow(y1 - y, 2) > pow(r, 2):
+            ans2 = True
             break
 
 
-if flag:
-    print('YES')
-else:
-    print('NO')
+print('YES' if ans1 else 'NO')
+print('YES' if ans2 else 'NO')
