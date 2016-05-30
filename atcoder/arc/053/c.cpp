2ff213239a6b93a514b4aecafc2e21907663d109
himkt <himkt@klis.tsukuba.ac.jp>
2016-05-30 16:47:55 +0900

    [ARC] Create 053/c #24

--- atcoder/arc/053/c.cpp
+++ atcoder/arc/053/c.cpp
@@ -19,15 +19,10 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define irep(i, n) for (int i = int(n) - 1; i >= 0; i--)
  
-int N;
-int a[100000];
-int b[100000];
+int N, a, b;
 
-int current, max_n;
-int ans = 0;
-
-pair<int, int> D[10000];
-pair<int, int> U[10000];
+pair<int, int> D[100000];
+pair<int, int> U[100000];
 
 int main() {
   std::cin.tie(0);
@@ -35,36 +30,40 @@ int main() {
 
   cin >> N;
 
+  int current = 0, ans = 0;
   int x = 0, y = 0;
 
   rep(i, N) {
-    cin >> a[i] >> b[i];
+    cin >> a >> b;
 
-    if (b[i] - a[i] > 0) {
-      D[x].first = a[i];
-      D[x].second = b[i];
+    // bのほうが大きい （唱えたあとで温度が減少する）
+    if (b - a > 0) {
+      D[x].first = a;
+      D[x].second = b;
       x++;
     }
     else {
-      U[y].first = b[i];
-      U[y].second = a[i];
+      U[y].first = b;
+      U[y].second = a;
       y++;
     }
 
-    sort(D, D+x+1);
-    sort(U, U+y+1);
-
-    rep (i, x+1) {
-      current += D[i].first;
-      if (ans < current) ans = current;
-      current -= D[i].second;
-    }
-
-    irep (i, y+1) {
-      current += U[i].second;
-      if (ans < current) ans = current;
-      current -= U[i].first;
-    }
   }
+
+  sort(D, D+x+1);
+  sort(U, U+y+1);
+
+  rep (i, x+1) {
+    current += D[i].first;
+    ans = max(current, ans);
+    current -= D[i].second;
+  }
+
+  irep (i, y+1) {
+    current += U[i].second;
+    ans = max(current, ans);
+    current -= U[i].first;
+  }
+
   cout << ans << endl;
 }
