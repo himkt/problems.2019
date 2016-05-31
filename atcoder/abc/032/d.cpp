c0c8e435cb6fdb5f602ed89e220904d29e8ec9b7
himkt <himkt@klis.tsukuba.ac.jp>
2016-06-01 00:19:03 +0900

    [ABC] 032 (how can I create dp table...) #23

--- atcoder/abc/032/d.cpp
+++ atcoder/abc/032/d.cpp
@@ -13,43 +13,34 @@
 # include <stack>
 # include <string>
 # include <vector>
-
+ 
 # define rep(i, n) for (int i = 0; i < (int)(n); i++)
 # define irep(i, n) for (int i = int(n) - 1; i >= 0; i--)
-
+ 
 using namespace std;
 const int INF = 1e8;
-
+ 
 int factorial(int i) {
   if (i < 1) return 1;
   return i * factorial(i-1);
 }
-
+ 
 int permutation(int k, int c) {
   return factorial(k) / factorial(k - c);
 }
-
+ 
 int combination(int k, int c) {
   return factorial(k) / (factorial(c) * factorial(k - c));
 }
-
-int n, W;
+ 
+int N, W;
 int v[200], w[200];
-long long dp[201][201];
-
-
+long long res;
+ 
 long long dfs(int i, int j) {
-
   cout << j << endl;
-
-  long long res;
-
-  if (dp[i][j] >= 0) {
-    return dp[i][j];
-  }
-
-
-  if (i == n) {
+ 
+  if (i == N) {
     res = 0;
   }
   else if (j < w[i]) {
@@ -58,24 +49,23 @@ long long dfs(int i, int j) {
   else {
     res = max(dfs(i+1, j), dfs(i+1, j-w[i])+v[i]);
   }
-
-  return dp[i][j] = res;
+ 
+  return res;
 }
-
+ 
+ 
 int main() {
   std::cin.tie(0);
   std::ios::sync_with_stdio(false);
-
-  cin >> n >> W;
-
-  memset(dp, -1, sizeof(dp));
-
-  rep(i, n) {
+ 
+  cin >> N >> W;
+ 
+  rep(i, N) {
     int a, b;
     cin >> a >> b;
-    v[i] = b;
-    w[i] = a;
+    v[i] = a;
+    w[i] = b;
   }
-
+ 
   cout << dfs(0, W) << endl;
 }
