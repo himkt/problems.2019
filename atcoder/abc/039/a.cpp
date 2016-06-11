a234b9f4775835c09c0f0486f5e21bb554834d00
himkt <himkt@klis.tsukuba.ac.jp>
2016-06-12 07:50:29 +0900

    [ABC] 039

---
+++ atcoder/abc/039/a.cpp
@@ -0,0 +1,51 @@
+# include <algorithm>
+# include <cassert>
+# include <cctype>
+# include <cstdio>
+# include <cstdlib>
+# include <cstring>
+# include <cmath>
+# include <iostream>
+# include <map>
+# include <queue>
+# include <set>
+# include <sstream>
+# include <stack>
+# include <string>
+# include <vector>
+
+# define rep(i, n) for (int i = 0; i < (int)(n); i++)
+# define irep(i, n) for (int i = int(n) - 1; i >= 0; i--)
+
+typedef long long ll;
+
+// N is size of row
+// M is size of column
+// vector< vector<int> > Matrix(N, vector<int>(M, -1));
+
+using namespace std;
+const int INF = 1e8;
+
+int factorial(int i) {
+  if (i < 1) return 1;
+  return i * factorial(i-1);
+}
+
+int permutation(int k, int c) {
+  return factorial(k) / factorial(k - c);
+}
+
+int combination(int k, int c) {
+  return factorial(k) / (factorial(c) * factorial(k - c));
+}
+
+int a, b, c;
+
+int main() {
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+
+  cin >> a >> b >> c;
+  ll ans = 2 * a*b + 2 * b*c + 2 * c*a;
+  cout << ans << endl;
+}
