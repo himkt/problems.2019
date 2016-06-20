bd1dde2cff9bf25e5da6e935d95ce0366e391161
himkt <himkt@klis.tsukuba.ac.jp>
2016-06-20 16:37:30 +0900

    DUMP

--- atcoder/abc/032/d.cpp
+++ atcoder/abc/032/d.cpp
@@ -16,73 +16,39 @@
  
 # define rep(i, n) for (int i = 0; i < (int)(n); i++)
 # define irep(i, n) for (int i = int(n) - 1; i >= 0; i--)
+# define FOR(i, m, n) for (int i = int(m); i < (int)(n); i++)
  
 using namespace std;
 const int INF = 1e8;
- 
-int factorial(int i) {
-  if (i < 1) return 1;
-  return i * factorial(i-1);
-}
- 
-int permutation(int k, int c) {
-  return factorial(k) / factorial(k - c);
-}
- 
-int combination(int k, int c) {
-  return factorial(k) / (factorial(c) * factorial(k - c));
-}
- 
 
-long long max(long long a, long long b) {
-  return a > b ? a : b;
-}
+typedef long long ll;
 
+ll N, W;
+ll v[200], w[200];
+ll dp[200 + 1][20 * 1000];
 
-int N;
-long long W;
-long long v[200], w[200];
-long long dp[201][1001];
-
-
-long long rec(int i, int j) {
-  long long res;
-
-  if (dp[i][j] >= 0) {
-    return dp[i][j];
+ll rec(){
+  for (int i = N - 1; i >= 0; i--) {
+    for (int j = 0; j <= W; j++) {
+      if (j < w[i]) {
+        dp[i][j] = dp[i + 1][j];
+      } else {
+        dp[i][j] = max(dp[i + 1][j], dp[i + 1][j - w[i]] + v[i]);
+      }
+    }
   }
-
-  if (i == N) {
-    res = 0;
-  }
-  else if (j < w[i]) {
-    res = rec(i+1, j);
-  }
-  else {
-    res = max(rec(i + 1, j), rec(i + 1, j - w[i]) + v[i]);
-  }
-
-  return dp[i][j] = res;
+  return dp[0][W];
 }
+
+
 
 int main() {
-  std::cin.tie(0);
-  std::ios::sync_with_stdio(false);
 
   cin >> N >> W;
 
   rep (i, N) {
-    long long a, b;
-    cin >> a >> b;
-    v[i] = a;
-    w[i] = b;
+    cin >> v[i] >> w[i];
   }
 
-  rep (i, 201) {
-    rep (j, 1001) {
-      dp[i][j] = -1;
-    }
-  }
-
-  cout << rec (0, W) << endl;
+  cout << rec() << endl;
 }
