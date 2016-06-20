14d84faaa9495110c1986f0d85413f216b7e4e4a
himkt <himkt@klis.tsukuba.ac.jp>
2016-06-20 13:24:45 +0900

    [ARC] Solve 054/a...

--- atcoder/arc/054/a.cpp
+++ atcoder/arc/054/a.cpp
@@ -13,49 +13,26 @@
 # include <stack>
 # include <string>
 # include <vector>
-
-# define rep(i, n) for (int i = 0; i < (int)(n); i++)
-# define irep(i, n) for (int i = int(n) - 1; i >= 0; i--)
+# include <iomanip>
 
 using namespace std;
-const int INF = 1e8;
+const int INF = 1001001001;
 
-int factorial(int i) {
-  if (i < 1) return 1;
-  return i * factorial(i-1);
-}
+int main(int argc, char const *argv[]) {
+  int l, x, y, s, d;
+  int length_1, length_2;
+  double ans;
+  cin >> l >> x >> y >> s >> d;
 
-int permutation(int k, int c) {
-  return factorial(k) / factorial(k - c);
-}
+  length_1 = (l + d - s) % l;
+  length_2 = (l + s - d) % l;
 
-int combination(int k, int c) {
-  return factorial(k) / (factorial(c) * factorial(k - c));
-}
-
-
-int main() {
-  std::cin.tie(0);
-  std::ios::sync_with_stdio(false);
-  int L, X, Y, S, D;
-  double forward, backward;
-
-  cin >> L >> X >> Y >> S >> D;
-
-  if (D >= S) {
-    forward = D - S;
-    backward = L - forward;
+  if ( x <= y ) {
+    ans = min((double)length_1 / (x + y), (double)length_2 / (y - x));
   }
   else {
-    backward = S - D;
-    forward = L - backward;
+    ans = double(length_1) / (x + y);
   }
 
-  double ans1, ans2, ans;
-  ans1 = forward / (X+Y);
-  ans2 = backward / (X-Y);
-
-  cout << ans1 << endl << ans2 << endl;
-
-  cout << min(ans1, ans2) << endl;
+  cout << fixed << setprecision(10) << ans << endl;
 }
