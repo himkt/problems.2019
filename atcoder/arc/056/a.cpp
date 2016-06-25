4703a2fcb825443b7c0fd2d98d380cd27ebfd3e5
himkt <himkt@klis.tsukuba.ac.jp>
2016-06-25 23:38:49 +0900

    [ARC] 056/a

---
+++ atcoder/arc/056/a.cpp
@@ -0,0 +1,39 @@
+# include <algorithm>
+# include <cassert>
+# include <cctype>
+# include <cstdio>
+# include <cstdlib>
+# include <cstring>
+# include <cmath>
+# include <iostream>
+# include <map>
+# include <queue>
+# include <set>
+# include <sstream>
+# include <stack>
+# include <string>
+# include <vector>
+# include <iomanip>
+
+# define rep(i, n) for (int i = 0; i < (int)(n); i++)
+# define irep(i, n) for (int i = int(n) - 1; i >= 0; i--)
+# define FOR(i, m, n) for (int i = int(m); i < (int)(n); i++)
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int INF = 1001001001;
+
+int a, b, k, l, ans, num;
+
+int main() {
+  cin >> a >> b >> k >> l;
+
+  int num1 = k % l;
+  int num2 = (k - num1) / l;
+  int ans = b * num2;
+  ans += min(a * num1, b);
+
+  cout << ans << endl;
+}
