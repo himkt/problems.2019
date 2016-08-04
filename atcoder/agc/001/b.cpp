1212bfe8dc0c330c3ecb4814c58471235b8c3305
himkt <himkt@klis.tsukuba.ac.jp>
2016-08-05 00:55:20 +0900

    [AGC] 001/b #25

---
+++ atcoder/agc/001/b.cpp
@@ -0,0 +1,35 @@
+# include <algorithm>
+# include <cassert>
+# include <cctype>
+# include <cstdio>
+# include <cstdlib>
+# include <cstring>
+# include <cmath>
+# include <iostream>
+# include <map>
+# include <queue>
+# include <set>
+# include <sstream>
+# include <stack>
+# include <string>
+# include <vector>
+# include <iomanip>
+
+# define rep(i, n) for (int i = 0; i < (int)(n); i++)
+# define irep(i, n) for (int i = int(n) - 1; i >= 0; i--)
+# define FOR(i, m, n) for (int i = int(m); i < (int)(n); i++)
+
+using namespace std;
+typedef long long ll;
+
+ll n, x;
+
+
+int f(ll n, ll x) {
+  return n - f(n-x, x);
+}
+
+int main() {
+  cin >> n >> x;
+
+}
