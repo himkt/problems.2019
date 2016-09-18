b58887163a32b5ee4ad10d113b0811388aae6b3b
himkt <himkt@klis.tsukuba.ac.jp>
2016-09-19 00:13:17 +0900

    [ARC] :zap: Add 009/b

--- atcoder/arc/009/b.cpp
+++ atcoder/arc/009/b.cpp
@@ -21,82 +21,78 @@
 
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> P;
 
-int index(vector<int> arr, int target) {
-  rep (i, arr.size()) if (arr[i] == target) return i;
-  return -1;
-}
 
-int pow(int n, int k) {
-  int res = n;
-  rep (i, k - 1) res *= n;
-  return res;
-}
+// %% NOTE : begin matrix %%
 
-pair<int, int> compare(int a, int b, vector<int> order) {
-  double num_a = log10(a);
-  int digit_a = num_a + 1;
+typedef vector<int> vec;
+typedef vector<vec> mat;
+vector<char> order(10);
 
-  double num_b = log10(b);
-  int digit_b = num_b + 1;
-
-  if (digit_a > digit_b) {
-    return make_pair(a, b);
-  }
-  else if (digit_b > digit_a) {
-    return make_pair(b, a);
-  }
-
-  // 桁ごとに比較していく
-  // 上から比較して決着したタイミングでreturnすれば良い
-
-  for (int i = digit_a; i >= 0; i--) {
-
-    // n桁目の数が取れていない (今は上位n桁の数が取れてしまっている)
-    int num_a_k = a / pow(10, i-1) % pow(10, i-1);
-    int num_b_k = b / pow(10, i-1) % pow(10, i-1);
-
-    // vector<int> aで定義されている数値ごとの順序関係で比較
-    // a.find(1), a.find(2) で比較するとかすると良さそう
-    if (index(order, num_a_k) > index(order, num_b_k)) {
-      return make_pair(a, b);
+void pretty_print(mat g) {
+  rep (i, g.size()) {
+    rep (j, g[0].size()-1) {
+      cout << g[i][j] << ' ';
     }
-    else if (index(order, num_b_k) > index(order, num_a_k)) {
-      return make_pair(b, a);
+    cout << g[i][g[0].size()-1] << endl;
+  }
+}
+
+
+mat mul(mat A, mat B) {
+  mat C(A.size(), vec(B.size()));
+  rep (i, A.size()) {
+    rep (k, B.size()) {
+      rep (j, B[0].size()) {
+        C[i][j] += A[i][k] * B[k][j];
+      }
     }
   }
-
-  return make_pair(a, b);
+  return C;
 }
 
-vector<int> sort(vector<int> a, vector<int> order) {
-  vector<int> res = a;
+// %% NOTE : end matrix %%
 
-  rep (i, res.size()-1) {
-    for (int j = res.size() - 1; j > i; j--) {
-      pair<int, int> ab = compare(res[j-1], res[j], order);
-      res[j-1] = ab.first;
-      res[j] = ab.second;
+
+// string[i] is char...
+bool compare(string a, string b) {
+
+  if (a.size() != b.size()) {
+    return a.size() < b.size();
+  }
+
+  rep (i, a.size()) {
+    if (a[i] != b[i]) {
+      rep (j, order.size()) {
+        if (a[i] == order[j]) return true;
+        if (b[i] == order[j]) return false;
+      }
     }
   }
 
-  return res;
+  return false;
 }
+
+
+
 
 int main() {
-  int N;
-  vector<int> order(10);
-  rep (i, 10) cin >> order[i];
+  rep (i, 10) {
+    cin >> order[i];
+  }
+  
+  int n;
+  cin >> n;
 
-  cin >> N;
+  vector<string> nums(n);
 
-  vector<int> a(N);
-  rep (i, N) cin >> a[i];
+  rep (i, n) {
+    cin >> nums[i];
+  }
 
-  vector<int> res = sort(a, order);
- 
-  rep (i, res.size()) {
-    cout << res[res.size() - i - 1] << endl;
+  sort(nums.begin(), nums.end(), compare);
+
+  rep (i, nums.size()) {
+    cout << nums[i] << endl;
   }
 }
