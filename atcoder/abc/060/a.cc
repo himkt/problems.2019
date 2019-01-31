4d679a20acc7e3b58c1bd03f8c3c7e6d50fab456
himkt <himkt@cookpad.com>
2019-01-31 23:34:06 +0900

    Add ABC060: A, B

---
+++ atcoder/abc/060/a.cc
@@ -0,0 +1,56 @@
+// include headers{{{
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+/*}}}*/
+# define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+
+template<typename integer>
+integer __lcm(integer a, integer b) {
+    return (a * b) / __gcd(a, b);
+}
+
+int main() {
+    string a, b, c;
+    cin >> a >> b >> c;
+
+    if (a.back() == b[0] and b.back() == c[0]) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
+
---
+++ atcoder/abc/060/b.cc
@@ -0,0 +1,58 @@
+// include headers{{{
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+/*}}}*/
+# define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+
+template<typename integer>
+integer __lcm(integer a, integer b) {
+    return (a * b) / __gcd(a, b);
+}
+
+int main() {
+    long long a, b, c;
+    cin >> a >> b >> c;
+    
+    string ans = "NO";
+    rep (i, a*b) {
+        if (i*a % b == c) {
+            ans = "YES";
+            break;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
+
