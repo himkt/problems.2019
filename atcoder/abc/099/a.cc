f5a228b1489f124422e016d4cc70906530e4e10b
himkt <himkt@cookpad.com>
2019-05-09 22:23:59 +0900

    Solve 099: A, B

---
+++ atcoder/abc/099/a.cc
@@ -0,0 +1,11 @@
+# include <string>
+# include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    if (n <= 999) cout << "ABC" << endl;
+    else cout << "ABD" << endl;
+    return 0;
+}
+
---
+++ atcoder/abc/099/b.cc
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int a, b; cin >> a >> b;
+    cout << (((b-a-1)*(b-a))/2)-a << endl;
+    return 0;
+}
+
