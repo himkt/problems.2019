92e308fd6330479c29bab5986d02f69aaf4d8e46
himkt <himkt@cookpad.com>
2019-11-23 21:20:29 +0900

    [ABC144] Solve A, B

---
+++ atcoder/abc/144/a.cc
@@ -0,0 +1,13 @@
+# include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+    int a, b; cin >> a >> b;
+    if (a >= 10 or b >= 10) cout << "-1" << endl;
+    else cout << a*b << endl;
+    return 0;
+}
+
