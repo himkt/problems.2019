337aa3300e48fd10735bf24d70df4c26d30ffe49
himkt <himkt@cookpad.com>
2019-11-30 23:08:38 +0900

    Cleanup ABC042

--- atcoder/abc/042/c.cc
+++ atcoder/abc/042/c.cc
@@ -8,33 +8,33 @@
 #include <string>
 #include <vector>
 #include <cmath>
-
-
+ 
+ 
 using namespace std;
-
-
+ 
+ 
 int to_int(char c) {
     return c - '0';
 }
-
-
+ 
+ 
 int main() {
     int n, k; cin >> n >> k;
-
+ 
     vector<int> d(k);
     for (int i=0; i<k; ++i) cin >> d[i];
-
+ 
     bool is_valid;
     for (int i=n; i<=10*n; ++i) {
-
+ 
         is_valid = true;
-
+ 
         for (char c: to_string(i)) {
             if (find(d.begin(), d.end(), to_int(c)) != d.end()) {
                 is_valid = false; break;
             }
         }
-
+ 
         if (is_valid) {
             cout << i << endl;
             return 0;
@@ -42,4 +42,3 @@ int main() {
     }
     return 0;
 }
-
