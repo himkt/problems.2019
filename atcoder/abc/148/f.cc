5c9e20443c4a2781e0b6bc8e1ee3ed7bac28acd9
himkt <himkt@cookpad.com>
2019-12-23 18:54:38 +0900

    [ABC148] Solve F

--- atcoder/abc/148/f.cc
+++ atcoder/abc/148/f.cc
@@ -29,44 +29,44 @@ int main() {
         return 0;
     }
 
-    vector<int> cost(n, 0);
+    vector<int> dist1(n, 0);
     queue<pair<int, int>> que;
     que.push(make_pair(v, 0));
     while (!que.empty()) {
         auto p = que.front(); que.pop();
         int cur = p.first;
         int cst = p.second;
-        cost[cur] = cst;
+        dist1[cur] = cst;
 
         for (int next_n: g[cur]) {
-            if (next_n == v or cost[next_n] > 0) continue;
-            que.push(make_pair(next_n, cst+1));
+          if (next_n == v or dist1[next_n] > 0)
+            continue;
+          que.push(make_pair(next_n, cst + 1));
         }
     }
 
-    vector<int> cost2(n, 0);
+    vector<int> dist2(n, 0);
     queue<pair<int, int>> que2;
     que2.push(make_pair(u, 0));
     while (!que2.empty()) {
         auto p = que2.front(); que2.pop();
         int cur = p.first;
-        int scr = p.second;
-        if (scr > n) break;
-
-        if (scr < cost[cur]) {
-            cost2[cur] = scr;
-        }
+        int cst = p.second;
+        dist2[cur] = cst;
 
         for (int next_n: g[cur]) {
-            if (scr >= cost[next_n]) continue;
-            que2.push(make_pair(next_n, scr+1));
+          if (next_n == u or dist2[next_n] > 0)
+            continue;
+          que2.push(make_pair(next_n, cst + 1));
         }
     }
 
-    int ans = -1;
-    for (int i=0; i<n; i++) ans = max(ans, cost2[i]);
-    if (ans == 0) ans++;
-    cout << ans << endl;
+    int ans = 0;
+    for (int i=0; i<n; i++) {
+        if (dist1[i] > dist2[i]) ans = max(ans, dist1[i]);
+    }
+
+    cout << ans-1 << endl;
     return 0;
 }
 
