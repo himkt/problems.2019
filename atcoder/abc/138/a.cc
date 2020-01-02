517afde4b28c159073c194cf3700b353e3c7baac
himkt <himkt@cookpad.com>
2020-01-02 21:37:59 +0900

    [ABC138] Solve A-D

---
+++ atcoder/abc/138/a.cc
@@ -0,0 +1,25 @@
+# include <cmath>
+# include <iostream>
+# include <queue>
+# include <set>
+# include <stack>
+# include <string>
+# include <vector>
+
+
+using namespace std;
+
+
+int main() {
+    int a; cin >> a;
+    string s; cin >> s;
+
+    if (a < 3200) {
+        cout << "red" << endl;
+    }
+    else {
+        cout << s << endl;
+    }
+    return 0;
+}
+
